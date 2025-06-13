d175bfd486d7f11827d58d83553038f41acbd02a
Patrick O'Grady <me@patrickogrady.xyz>
2025-06-13 10:19:08 -0700

    spike

--- local/src/server/mod.rs
+++ local/src/server/mod.rs
@@ -1,5 +1,7 @@
+use axum::{serve, Router};
 use std::path::PathBuf;
 use thiserror::Error;
+use tokio::net::TcpListener;
 
 /// Subcommand for the server.
 pub const CMD: &str = "server";
@@ -9,8 +11,20 @@ pub const RUN_CMD: &str = "run";
 
 /// Errors that can occur when running the local server.
 #[derive(Error, Debug)]
-pub enum Error {}
+pub enum Error {
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+}
 
 pub async fn run(directory: &PathBuf, port: &u16) -> Result<(), Error> {
-    unimplemented!()
+    // Create a listener for the server on the specified port.
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+
+    // Create a router for the server.
+    let router = Router::new();
+
+    // Serve the server.
+    serve(listener, router.into_make_service())
+        .await
+        .map_err(Error::Io)
 }
